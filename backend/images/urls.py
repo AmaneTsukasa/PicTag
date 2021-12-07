0f0c035a5089788a622f034730cf645097eac30e
amanetsu <amanetsukasa724@gmail.com>
2021-12-07 02:10:44 +0200

    Add tag search

--- backend/images/urls.py
+++ backend/images/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import ImageListView
 
+
 app_name='images'
 
 urlpatterns = [
--- backend/images/views.py
+++ backend/images/views.py
@@ -1,4 +1,7 @@
+from operator import attrgetter
+
 from django.views.generic import ListView
+from django.db.models import Count
 
 from .models import Image, Tag
 
@@ -6,9 +9,20 @@ class ImageListView(ListView):
     model = Image
     context_object_name = 'images'
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        q = self.request.GET.get('q', '')
+        if q:
+            qs = qs.filter(tags__name__in=q.split()).distinct()
+
+        return qs
+
     def get_context_data(self, object_list=None, **kwargs):
         context = super().get_context_data(object_list=object_list, **kwargs)
         if context['images']:
-            id_list = [obj.id for obj in context['images']]
-            context['tags'] = Tag.objects.filter(images__id__in=id_list).order_by('name').distinct()
+            tags_qs = Tag.objects.annotate(count=Count('images', distinct=True))
+            tags_qs = tags_qs.filter(images__id__in=map(attrgetter('id'), context['images']))
+            context['tags'] = tags_qs.order_by('name').distinct()
+        context['q'] = self.request.GET.get('q')
         return context
